9f2712dcab7531625f5f9830c407abadef1ea459
Pratik <khadkapratikk10@gmail.com>
2024-06-12 09:19:53 +0545

    feat: add theme context provider

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,36 +7,39 @@ import Home from './pages/Home.tsx';
 import Platform from './pages/Platform.tsx';
 import NotFound from './pages/NotFound.tsx';
 import AuthContextProvider from './store/auth-context.tsx';
+import ThemeContextProvider from './store/theme-context.tsx';
 import PrivateRoute from './components/PrivateRoute.tsx';
 import { PLATFORMS } from './data/platforms.ts';
 
 function App() {
   return (
     <AuthContextProvider>
-      <Layout>
-        <Routes>
-          <Route path='/register' element={<Register />}></Route>
-          <Route path='/login' element={<Login />}></Route>
-          <Route
-            path='/'
-            element={
-              <PrivateRoute>
-                <Home />
-              </PrivateRoute>
-            }
-          ></Route>
-          {PLATFORMS.map((platform) => {
-            return (
-              <Route
-                key={platform.id}
-                path={`/${platform.id}`}
-                element={<Platform />}
-              ></Route>
-            );
-          })}
-          <Route path='*' element={<NotFound />}></Route>
-        </Routes>
-      </Layout>
+      <ThemeContextProvider>
+        <Layout>
+          <Routes>
+            <Route path='/register' element={<Register />}></Route>
+            <Route path='/login' element={<Login />}></Route>
+            <Route
+              path='/'
+              element={
+                <PrivateRoute>
+                  <Home />
+                </PrivateRoute>
+              }
+            ></Route>
+            {PLATFORMS.map((platform) => {
+              return (
+                <Route
+                  key={platform.id}
+                  path={`/${platform.id}`}
+                  element={<Platform />}
+                ></Route>
+              );
+            })}
+            <Route path='*' element={<NotFound />}></Route>
+          </Routes>
+        </Layout>
+      </ThemeContextProvider>
     </AuthContextProvider>
   );
 }
