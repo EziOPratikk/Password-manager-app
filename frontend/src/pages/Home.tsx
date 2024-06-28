050d19f094d394727fcd6b7d4bd2fd2398dd11c7
Pratik <khadkapratikk10@gmail.com>
2024-06-28 20:08:19 +0545

    feat: add search functionality

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,15 +1,55 @@
-import PlatformItem from '../components/PlatformItem';
-import { PLATFORMS } from '../data/platforms';
+import { type ChangeEvent, type ReactNode, useState, useEffect } from 'react';
+
+import PlatformItem from '../components/PlatformItem.tsx';
+import PlatformItemSkeleton from '../components/PlatformItemSkeleton.tsx';
+import { PLATFORMS } from '../data/platforms.ts';
+
+import TextField from '@mui/material/TextField';
+import SearchIcon from '@mui/icons-material/Search';
+import { IconButton } from '@mui/material';
 
 export default function Home() {
-  return (
-    <div>
-      <h1 className='text-center text-xl font-bold mb-12'>
-        Manage Your Passwords
-      </h1>
+  const [platforms, setPlatforms] = useState(PLATFORMS);
+  const [searchedKeyword, setSearchedKeyword] = useState('');
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      setIsLoading(false);
+    }, 1000);
+
+    return () => clearTimeout(timeoutId);
+  }, []);
+
+  function handleSearchedKeyword(event: ChangeEvent<HTMLInputElement>) {
+    const inputKeyword = event.currentTarget.value;
+    setSearchedKeyword(inputKeyword);
+
+    if (!inputKeyword) {
+      setPlatforms(PLATFORMS);
+      return;
+    }
+  }
+
+  function searchPlatforms() {
+    if (!searchedKeyword) return;
+
+    const filteredPlatforms = PLATFORMS.filter((platform) =>
+      platform.keywords.includes(searchedKeyword.toLowerCase())
+    );
+
+    setPlatforms(filteredPlatforms);
+  }
+
+  let platformsGridContent: ReactNode;
+
+  if (platforms.length !== 0) {
+    platformsGridContent = (
       <ul className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8'>
-        {PLATFORMS.map((platform) => {
-          return (
+        {platforms.map((platform) => {
+          return isLoading ? (
+            <PlatformItemSkeleton key={platform.id} />
+          ) : (
             <PlatformItem
               key={platform.id}
               id={platform.id}
@@ -19,6 +59,40 @@ export default function Home() {
           );
         })}
       </ul>
+    );
+  } else {
+    platformsGridContent = (
+      <p className='text-center text-xl font-bold'>
+        Sorry, we couldn’t find any matches for "{searchedKeyword}" platform :(
+      </p>
+    );
+  }
+
+  return (
+    <div>
+      <h1 className='text-center text-xl font-bold mb-8 md:-mt-4'>
+        Manage Your Passwords
+      </h1>
+      <div className='text-center mb-8'>
+        <TextField
+          type='text'
+          size='small'
+          label='Search'
+          autoComplete='off'
+          InputProps={{ sx: { borderRadius: 50 } }}
+          value={searchedKeyword}
+          onChange={handleSearchedKeyword}
+        />
+        <IconButton
+          type='button'
+          aria-label='search'
+          sx={{ marginLeft: '-2.5rem' }}
+          onClick={searchPlatforms}
+        >
+          <SearchIcon />
+        </IconButton>
+      </div>
+      {platformsGridContent}
     </div>
   );
 }
